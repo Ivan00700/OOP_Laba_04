48263f2705680dfc47d025baacffd841175a685c
Ivan <ivanrazvodovok@gmail.com>
2025-11-07 20:11:24 +0300

    update commit fot Laba_04

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Иван/Documents/ООП/Laba_04
+# Build directory: /mnt/c/Users/Иван/Documents/ООП/Laba_04/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_figure "/mnt/c/Users/Иван/Documents/ООП/Laba_04/build/test_figure")
+set_tests_properties(test_figure PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Иван/Documents/ООП/Laba_04/CMakeLists.txt;26;add_test;/mnt/c/Users/Иван/Documents/ООП/Laba_04/CMakeLists.txt;0;")
